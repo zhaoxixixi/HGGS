784c4ea14534e9327824bc7c665e89ce4afbfc2d
Heng Rao <66891417+zhaoxixixi@users.noreply.github.com>
2024-08-11 14:17:24 +0800

    Update pre_produce_data.py
    author

--- src/yu/tools/pre_produce_data.py
+++ src/yu/tools/pre_produce_data.py
@@ -1,9 +1,4 @@
 '''
-Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
-Date: 2023-09-21 03:19:45
-LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
-LastEditTime: 2023-10-13 12:56:16
-FilePath: /rh/Sample/src/yu/tools/pre_produce_data.py
 '''
 import os.path
 
