38f53253df53a0041c9c3e2db9b0e9a432ed2ebd
Heng Rao <66891417+zhaoxixixi@users.noreply.github.com>
2024-08-11 14:10:24 +0800

    Update base.py
    erase author

--- src/yu/exception/base.py
+++ src/yu/exception/base.py
@@ -1,11 +1,3 @@
-"""
-@author: longlong.yu
-@email: yu.hz@qq.com
-@date: 2023-04-01 14:05
-@description: Base Exception
-"""
-
-
 class YuBaseException(Exception):
     """ base exception class """
     pass
