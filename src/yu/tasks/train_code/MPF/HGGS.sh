cd6819e4678e14b10a669cccd55c04af5a0a5a88
Heng Rao <66891417+zhaoxixixi@users.noreply.github.com>
2024-08-11 17:43:14 +0800

    Update HGGS.sh
    dropout=0.12

--- src/yu/tasks/train_code/MPF/HGGS.sh
+++ src/yu/tasks/train_code/MPF/HGGS.sh
@@ -9,5 +9,5 @@ python multi_seed_train.py [0,1,2,3,4,5] [6,7] "HGGS-1w" \
 --test_paths "../../../../data/6param/MPF_2_Var/val/val_5k.csv" \
 --xs_param "[0.01, 0.01, 10, 0.04, 100, 1.5, 0.1, 1]" --param_selected "[0,1,3,5,6,7]" \
 --nn_layers "[128, 128, 128, 128]" --nn_norm "[\"BatchNorm1d\",\"BatchNorm1d\",\"BatchNorm1d\",\"BatchNorm1d\"]" \
---total_training_samples 10000 --dropout 0.1 --batch_size 409600 \
+--total_training_samples 10000 --dropout 0.12 --batch_size 409600 \
 --Gaussian_Mixture True --Ada_Gradient_Settings "[\"None\", 5]"
