f12208f8c2102dcdf6ab9988cd4b7aec59b3efda
zhaoxixixi <66891417+zhaoxixixi@users.noreply.github.com>
2024-09-14 22:39:18 +0800

    bug in multi_seed_train.py

--- src/yu/tasks/BioSysConfig/BioSysConfig.py
+++ src/yu/tasks/BioSysConfig/BioSysConfig.py
@@ -12,11 +12,12 @@ class SysConfig():
             'Error! Only define `[brusselator, HSECC, MPF_2_Var, PS2_01]`'
         assert sampling_name in ['HGGS', 'IS', 'IS-dag', 'US-P', 'US-S', 'VeSSAL', 'WRS']
 
+        train_data_mode = "GG-Sampling" if sampling_name == 'HGGS' else "Baseline"
         self.sys_name = sys_name
         if sys_name == 'brusselator':
             self.xs = [0,1]
             self.ys = [2,3]
-            self.data_path = "/brusselator/train/GG-Sampling/"
+            self.data_path = f"/brusselator/train/{train_data_mode}/"
             self.ode_model_name = 'brusselator'
             self.test_path = "../../../../data/2param/brusselator/val/val_5k.csv"
             self.xs_param = [1, 3]
@@ -32,7 +33,7 @@ class SysConfig():
         elif sys_name == 'HSECC':
             self.xs = [0, 1, 2, 3, 4, 5]
             self.ys = [6, 7, 8]
-            self.data_path = "/HSECC/train/GG-Sampling/"
+            self.data_path = f"/HSECC/train/{train_data_mode}/"
             self.ode_model_name = 'HSECC'
             self.test_path = "../../../../data/6param/HSECC/val/val_5k.csv"
             self.xs_param = [1.53,0.04,1.35,0.02,1.35,0.1,0.00741]
@@ -48,7 +49,7 @@ class SysConfig():
         elif sys_name == 'MPF_2_Var':
             self.xs = [0, 1, 2, 3, 4, 5]
             self.ys = [6, 7]
-            self.data_path = "/MPF_2_Var/train/GG-Sampling/"
+            self.data_path = f"/MPF_2_Var/train/{train_data_mode}/"
             self.ode_model_name = 'MPF_2_Var'
             self.test_path = "../../../../data/6param/MPF_2_Var/val/val_5k.csv"
             self.xs_param = [0.01, 0.01, 10, 0.04, 100, 1.5, 0.1, 1]
@@ -64,7 +65,7 @@ class SysConfig():
         elif sys_name == 'PS2_01':
             self.xs = [0, 1, 2, 3, 4, 5]
             self.ys = [6, 7]
-            self.data_path = "/PS2_01/train/GG-Sampling/"
+            self.data_path = f"/PS2_01/train/{train_data_mode}/"
             self.ode_model_name = 'PS2_01'
             self.test_path = "../../../../data/6param/PS2_01/val/val_5k.csv"
             self.xs_param = [2.8, 0.1, 0.1, 1, 5, 1]
